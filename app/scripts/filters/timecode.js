93100a0950c150ad14c98098acc0cd057ed273ec
Natalie Wyll <nataliewyll@yahoo.com>
2017-08-11 12:56:59 -0500

    assignment-11: uses buzz toTimer method for timecode filter

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,25 +1,7 @@
 (function() {
   function timecode() {
     return function(seconds) {
-      var seconds = Number.parseFloat(seconds);
-
-      if(Number.isNaN(seconds)) {
-        return '-:--';
-      }
-
-      var wholeseconds = Math.floor(seconds),
-          minutes = Math.floor(wholeseconds/60),
-          remainingSeconds = wholeseconds % 60;
-
-      var output = minutes + ':';
-
-      if (remainingSeconds < 10 ) {
-        output += 0;
-      }
-
-      output += remainingSeconds;
-
-      return output;
+        return buzz.toTimer(seconds);
     };
   }
 
